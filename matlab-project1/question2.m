497bee678fda6ce3def49f7ca3eca3c5a1bdaff7
M. Emin Tutam <79085245+EminTutam@users.noreply.github.com>
2023-03-09 17:11:27 +0300

    Update question2.m

--- matlab-project1/question2.m
+++ matlab-project1/question2.m
@@ -2,11 +2,11 @@ number=input('The number of laptops = ');
 if number<0
     disp('Please enter a positive integer');
 elseif number<=100
-    disp(['The price of ' num2str(number) ' laptops are ' num2str(number*10000) 'TL.']);
+    disp(['The price of ' num2str(number) ' laptops is $' num2str(number*10000) '.']);
 elseif 100<number && number<200
-    disp(['The price of ' num2str(number) ' laptops are ' num2str(number*(10000-10000*10/100)) 'TL.']);
+    disp(['The price of ' num2str(number) ' laptops is $' num2str(number*(10000-10000*10/100)) '.']);
 elseif 200<=number && number<=500
-    disp(['The price of ' num2str(number) ' laptops are ' num2str(number*(10000-10000*15/100)) 'TL.']);
+    disp(['The price of ' num2str(number) ' laptops is $' num2str(number*(10000-10000*15/100)) '.']);
 elseif 500<number
     disp(['There are no ' num2str(number) ' laptops in stock.']);
 end
